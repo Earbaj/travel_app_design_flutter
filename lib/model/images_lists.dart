fb6002a0af4844a0bed3340464e096780755701d
Earbaj <earbajsaria3@gmail.com>
2021-08-15 22:25:56 +0600

    Add Helper class to store all image link on an array

---
+++ lib/model/images_lists.dart
@@ -0,0 +1,24 @@
+class Images{
+  List<String> images_cox = [
+    "https://media-eng.dhakatribune.com/uploads/2020/03/"
+        "cox-s-bazaar-syed-zakir-hossain-1584366863439.jpg",
+    "https://tbsnews.net/sites/default/files/styles/big_2/"
+        "public/images/2020/06/15/coxs-bazar.jpg?itok=TSnIWCl7&timestamp=1599740004",
+    "https://media-cdn.tripadvisor.com/media/photo-c/"
+        "2560x500/10/e2/f8/43/longest-sea-beach-in.jpg",
+    "http://2.bp.blogspot.com/-17NgkznLIPo/TcJOoUBEtiI/"
+        "AAAAAAAAAAY/ETj5qryjLZ0/s1600/2009-04-18__coxs-bazar.jpg",
+    "https://thefinancialexpress.com.bd/public/uploads/1596726755.jpg",
+    "https://upload.wikimedia.org/wikipedia/"
+        "commons/c/cf/Cox%27s_Bazaar_Sunset_Sep2019.jpg"
+  ];
+  List<String> images_kaptai = [
+    "https://www.travelmate.com.bd/wp-content/uploads/2019/07/Kaptai-Lake-2.jpg",
+    "https://tbsnews.net/sites/default/files/styles/big_2/public/images/2020/01/22/"
+        "rangamati_kaptai_lake_pic_1.jpg?itok=5k0GkpJN&timestamp=1579699382",
+    "https://www.travelinbd.com/wp-content/uploads/2018/01/14885730736_8ee6652e9e_b.jpg",
+    "https://www.localguidesconnect.com/t5/image/serverpage/image-id/689067iBC89EA45951EF223/image-size/large?v=v2&px=999",
+    "https://cdn.britannica.com/80/137580-050-9C6A745A/Hanging-Bridge-Kaptai-Lake-Rangamati-Bangl.jpg",
+    "https://2.bp.blogspot.com/-ekaRaHlk6DM/WgCwvlZQzwI/AAAAAAAAAaU/lrp7_GShUtIsn7jynSLURauXhDQagaKywCEwYBhgL/s400/64132277.jpg"
+  ];
+}
